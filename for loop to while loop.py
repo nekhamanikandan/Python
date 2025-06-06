98f41cf10ea8b82c851d81248aa3e9664c69f7c0
nekhamanikandan <nekhamanikandan31@gmail.com>
2025-06-06 11:44:01 +0530

    Add files via upload

---
+++ for loop to while loop.py
@@ -0,0 +1,43 @@
+
+for value in range(0,10):
+    print(value*"*")
+
+value=0
+while value<10:
+    print(value*"*")
+    value+=1
+
+for value in range(0,10):
+    print((9-value)*" "+value*' * ')
+
+
+value=0
+while value<10:
+    print((1-value)*" "+value*"*")
+    value+=1
+
+
+for value in range(0,5):
+    print((4-value)*"  "+value*"  *  ")
+
+value=0
+while value<5:
+    print((1-value)*"   "+value*"  * ")
+    value+=1
+
+
+
+for value in range(0,15):
+    print((2-value)*" "+value*"  *  ")
+
+value=0
+while value<15:
+    print((2-value)*"  "+value*"  *  ")
+    value+=1
+
+
+
+
+
+
+
