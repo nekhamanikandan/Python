2dcf9eb1d1d62a9aee833b376ca92b9e3288f362
nekhamanikandan <nekhamanikandan31@gmail.com>
2025-06-06 09:55:56 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,53 @@
+list1=[1,2,3,9,8,7,6,50,13]
+type(list1)
+for value in list1:
+    print(value)
+
+for index in range(0,len(list1)):
+    print(list1[index])
+
+index=0
+while index<len(list1):
+    print(list1[index])
+    index=index+1
+
+
+
+
+list2=[6,8,7,9,4,3,17,68,17,5,3,9]
+for index in range(0,len(list2)):
+    print(list2[index])
+
+
+index=0
+while index<len(list2):
+    print(list2[index])
+    index=index+1
+
+
+list3=[5,4,7,6,90,2,3,5,6,7,21]
+for index in range(0,len[list3]):
+    print(list3[index])
+index=0
+while index<len(list3):
+    print(list3[index])
+    index=index+1
+
+
+index=len(list3)-1
+while index>=0:
+    print(list3[index])
+    index=index-1
+
+index=0
+while index<len(list3):
+    print
+
+
+for value in range(0,len(list3)):
+    if list3[value]%2==0:
+        print("even number",list3[value])
+    else:
+        print("odd number",list3[value])
+
+
