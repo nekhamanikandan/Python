06da09e06ce5231932c0aae13f5fb6785e147d99
nekhamanikandan <nekhamanikandan31@gmail.com>
2025-06-10 11:35:36 +0530

    Add files via upload

--- while loop.py
+++ while loop.py
@@ -1,53 +1,130 @@
-list1=[1,2,3,9,8,7,6,50,13]
+list1=[]
 type(list1)
-for value in list1:
-    print(value)
-
-for index in range(0,len(list1)):
-    print(list1[index])
-
-index=0
-while index<len(list1):
-    print(list1[index])
-    index=index+1
+for value in range(0,10):
+    list1.append(input("pls enter value"))
+list1
 
 
+list2=[]
+for value in range(0,15):
+    list2.append(input("pls enter value"))
+print(list2)  
+
+list3=[]
+for value in range(0,5):
+    list3.append(input("pls enter value"))
+print(list3)   
+list3
+
+list1=[]
+value=0
+while value<10:
+    list1.append(input("pls enter value"))
+    print(list1)
+    value+=1
+
+list1
 
 
-list2=[6,8,7,9,4,3,17,68,17,5,3,9]
-for index in range(0,len(list2)):
-    print(list2[index])
+list2=[]
+value=0
+while value<9:
+    list2.append(input("pls enter vale"))
+    print(list2)
+    value+=1
 
 
-index=0
-while index<len(list2):
-    print(list2[index])
-    index=index+1
 
 
-list3=[5,4,7,6,90,2,3,5,6,7,21]
-for index in range(0,len[list3]):
-    print(list3[index])
-index=0
-while index<len(list3):
-    print(list3[index])
-    index=index+1
+list3=[]
+value=input("not enter zero")
+while value!='0':
+    list3.append(value)
+    print(list3)
+    value=input("not enter zero")
+
+list3
+
+list4=[]
+value=input("not enter zero")
+while value!='0':
+    list4.append(value)
+    print(list4)
+    value=input("not enter zero")
+
+list4
 
 
-index=len(list3)-1
-while index>=0:
-    print(list3[index])
-    index=index-1
+list5=[]
+value=input("not enter zero")
+while value!='0':
+    list5.append(value)
+    print(list5)
+    value=input("not enter zero")
 
-index=0
-while index<len(list3):
-    print
-
-
-for value in range(0,len(list3)):
-    if list3[value]%2==0:
-        print("even number",list3[value])
-    else:
-        print("odd number",list3[value])
+list5
 
 
+list2=[]
+value=int(input(" not enter zero"))
+result=0
+while value !=0:
+    list2.append(value)
+    result=result+value
+    print(list2)
+    value=int(input("not enter zero"))
+result
+list2
+list1
+
+list1=[]
+value=int(input("not enter zero"))
+result=0
+while value!=0:
+    list1.append(value)
+    result=result+value
+    print(list1)
+    value=int(input("not enter zero"))
+result
+list1
+
+
+list2=[]
+value=int(input("not enter zero"))
+result=1
+while value !=0:
+    list2.append(value)
+    result=result*value
+    print(list2)
+    value=int(input("npt enter zero"))
+result
+list2
+
+
+
+list7=[]
+value=int(input("not enter zero"))
+result=0
+while value !=0:
+    list7.append(value)
+    result=result+value
+    if result>100:
+        break
+    print(list7)
+    value=int(input("not enter zero"))
+result
+list7
+
+
+list1=[]
+value=int(input("not enter zero"))
+result=0
+while value !=0:
+    list1.append(value)
+    result=result+value
+    if result>100:
+        break
+    print(list1)
+    value=int(input("not enter zero"))
+result
+list1
